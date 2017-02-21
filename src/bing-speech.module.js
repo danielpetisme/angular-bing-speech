05c56d81f7f6f843a96822ae66d2238a61973b91
Daniel Petisme <daniel.petisme@gmail.com>
2017-02-21 22:19:40 +0100

    Add HTTP Interceptor

--- src/bing-speech.module.js
+++ src/bing-speech.module.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2017 Daniel Petisme
 // Licensed under the MIT license
 
-(function() {
+(function () {
     'use strict';
     angular
-        .module('bing-speech', ['bing-speech.auth', 'bing-speech.service']);        
+        .module('bing-speech', ['bing-speech.auth', 'bing-speech.service'])
+        .config(httpConfig);
+        
+        httpConfig.$inject = ['$httpProvider', 'jwtOptionsProvider'];
+
+        function httpConfig($httpProvider, jwtOptionsProvider) {            
+            jwtOptionsProvider.config({
+                tokenGetter: ['BingSpeechAuth', function (BingSpeechAuth) {
+                    return BingSpeechAuth.login();
+                }]
+            });
+
+            $httpProvider.interceptors.push('jwtInterceptor');
+        });
 })();
